cd49b6b8249a928c0a878fa7b6409621989cc3ac
Gilles Renevey <Gilles Renevey>
2016-01-17 23:11:30 +0100

    Add personalDataForm class

---
+++ src/personalDataForm.ts
@@ -0,0 +1,37 @@
+import { Component } from 'angular2/core';
+import {
+	FormBuilder,
+	Validators,
+	Control,
+	ControlGroup,
+	FORM_DIRECTIVES
+} from 'angular2/common';
+
+import { FormValidator } from './FormValidator.ts'
+
+@Component({
+  // Declare the tag name in index.html to where the component attaches
+  selector: 'personalDataForm',
+  // Location of the template for this component
+  templateUrl: 'src/personalDataForm.html',
+  directives: [FORM_DIRECTIVES]
+})
+
+export class PersonalDataForm {
+    
+    birthDate: string = '';   
+    civilStatus = ['Celibataire', 'Marie', 'Divorcé'];
+    nationality = ['Suisse', 'France', 'Allemagne'];
+    kids = [1,2,3,4,5,6,7,8,9];
+    retireAge = ['2 ans avant l\'âge de la retraite légal', 'âge de la retraite légal'];
+    
+    birthDateControl = new Control(
+        this.birthDate,
+        Validators.compose([Validators.required, FormValidator.isNotDate])
+    );
+
+    formGroup: ControlGroup = new ControlGroup({
+      birthDateControl: this.birthDateControl
+      //, civilStatusControl: this.civilStatusControl
+    });
+};
